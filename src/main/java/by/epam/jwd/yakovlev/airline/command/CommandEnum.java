a027714e53b4359ce591fbf2a84b0b69e8db7e81
Alexander Yakovlev <sashayassa@gmail.com>
2020-04-06 17:35:17 +0300

    Add new dommands to the CommandEnum

--- src/main/java/by/epam/jwd/yakovlev/airline/command/CommandEnum.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/command/CommandEnum.java
@@ -6,11 +6,14 @@ public enum CommandEnum {
     INITIALISE (new Initialise()),
     REFRESH_PAGE (new RefreshPage()),
     SET_LANGUAGE (new SetLanguage()),
-    GOTO_PAGE (new GotoPage()),
+    GOTO_PAGE_LOGIN (new GotoPageLogin()),
+    GOTO_PAGE_AIRCRAFTS (new GotoPageAircrafts()),
+    HOME (new Home()),
     LOGIN_USER (new LoginUser()),
     LOGOUT_USER (new Logout()),
     REGISTER_USER (new RegisterUser()),
-    ADD_AIRCRAFT (new AddAircraft());
+    ADD_AIRCRAFT (new AddAircraft()),
+    MANAGE_EMPLOYEES (new ManageEmployees());
 
     Command command;
 
