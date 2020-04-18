d06e9d0d231ac22dcd0024c0d05b7e6b70bae510
Alexander Yakovlev <sashayassa@gmail.com>
2020-04-18 12:55:39 +0300

    add command exception

---
+++ src/main/java/by/epam/jwd/yakovlev/airline/exception/CommandException.java
@@ -0,0 +1,23 @@
+package by.epam.jwd.yakovlev.airline.exception;
+
+public class CommandException extends Error{
+
+	public CommandException() {
+    }
+
+    public CommandException(String message) {
+        super(message);
+    }
+
+    public CommandException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CommandException(Throwable cause) {
+        super(cause);
+    }
+
+    public CommandException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
