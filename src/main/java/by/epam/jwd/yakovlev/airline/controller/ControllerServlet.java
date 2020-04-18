372f29f7b482d5f20ab1bf9fc885555c5fcd23ee
Alexander Yakovlev <sashayassa@gmail.com>
2020-04-18 12:51:19 +0300

    udate controller servlet

--- src/main/java/by/epam/jwd/yakovlev/airline/controller/ControllerServlet.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/controller/ControllerServlet.java
@@ -2,78 +2,109 @@ package by.epam.jwd.yakovlev.airline.controller;
 
 import by.epam.jwd.yakovlev.airline.command.Command;
 import by.epam.jwd.yakovlev.airline.command.CommandEnum;
+import by.epam.jwd.yakovlev.airline.entity.Employee;
 import by.epam.jwd.yakovlev.airline.exception.AirlineDataBaseConnectionException;
+import by.epam.jwd.yakovlev.airline.exception.ServiceException;
+import by.epam.jwd.yakovlev.airline.exception.ServiceIsUnavailableException;
 import by.epam.jwd.yakovlev.airline.pool.ConnectionsPool;
+import by.epam.jwd.yakovlev.airline.service.EmployeeService;
+import by.epam.jwd.yakovlev.airline.service.ServiceFactory;
 import by.epam.jwd.yakovlev.airline.util.StringConstant;
 
 import org.apache.log4j.Logger;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet(name = "ControllerServlet", value = "/")
 public class ControllerServlet extends HttpServlet {
 
-    private static final Logger LOGGER = Logger.getLogger(ControllerServlet.class);
+	private static final Logger LOGGER = Logger.getLogger(ControllerServlet.class);	
 
-    @Override
-    public void init() throws ServletException {
-        super.init();
-        try {
-            ConnectionsPool.INSTANCE.initializePool();
-        } catch (AirlineDataBaseConnectionException e) {
-            LOGGER.warn("Pool of connection is not initialise");
-        }
-    }
+	@Override
+	public void init() throws ServletException {
+		super.init();
 
-    @Override
-    public void destroy() {
-        super.destroy();
-        ConnectionsPool.INSTANCE.close();
-    }
+		LOGGER.debug("Init is started");
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+		try {
+			ConnectionsPool.INSTANCE.initializePool();
+		} catch (AirlineDataBaseConnectionException e) {
+			LOGGER.warn("Pool of connection is not initialise");
+		}
+	}
 
-        Command command = getCommand(request, response);
-        request.getRequestDispatcher(command.execute(request, response)).forward(request, response);
-    }
+	@Override
+	public void destroy() {
+		super.destroy();
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+		LOGGER.debug("Destroy is started");
 
-        Command command = getCommand(request, response);
+		ConnectionsPool.INSTANCE.close();
+	}
 
-        request.getRequestDispatcher(command.execute(request, response)).forward(request, response);
-    }
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    private Command getCommand(HttpServletRequest request, HttpServletResponse response) {
+		LOGGER.debug("doGet is started");
 
-        Command command = null;
-        HttpSession session = request.getSession( true);
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("utf-8");
 
-        if (session.getAttribute(StringConstant.CURRENT_SESSION_USER_KEY.getValue()) == null) {
-            return CommandEnum.INITIALISE.getCommand();
-        }
+		Command command = getCommand(request, response);
 
-        String commandName = request.getParameter(StringConstant.COMMAND_KEY.getValue());
+		LOGGER.debug("doGet got command " + command.getClass());
+		request.getRequestDispatcher(command.execute(request, response)).forward(request, response);
+	}
 
-        if (commandName == null) {
-            return CommandEnum.REFRESH_PAGE.getCommand();
-        }
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-        for (CommandEnum c : CommandEnum.values()) {
-            if (c.name().equals(commandName.toUpperCase())) {
-                command = c.getCommand();
-            }
-        }
+		LOGGER.debug("doPost is started");
 
-        LOGGER.debug("Controller servlet got the command " + commandName);
-        return command;
-    }
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("utf-8");
+
+		Command command = getCommand(request, response);
+		LOGGER.debug("doPost got command " + command.getClass());
+		request.getRequestDispatcher(command.execute(request, response)).forward(request, response);
+	}
+
+	private Command getCommand(HttpServletRequest request, HttpServletResponse response) {
+		
+		LOGGER.debug("getCommand is started");
+		LOGGER.debug(
+				"Controller servlet got the command " + request.getParameter(StringConstant.COMMAND_KEY.getValue()));
+		
+		String commandName = request.getParameter(StringConstant.COMMAND_KEY.getValue());
+		if (commandName == null) {
+			LOGGER.debug("command name is null. Return GOTO_PAGE_INDEX");
+			return CommandEnum.INITIALISE.getCommand();
+		}
+
+		HttpSession session = request.getSession();
+
+		session.setAttribute(StringConstant.WARNING_MESSAGE_KEY.getValue(), 
+				StringConstant.EMPTY_STRING.getValue());
+		session.setAttribute(StringConstant.SUCCESS_MESSAGE_KEY.getValue(), 
+				StringConstant.EMPTY_STRING.getValue());
+
+		Command command = null;
+		for (CommandEnum c : CommandEnum.values()) {
+			if (c.name().equals(commandName.toUpperCase())) {
+				command = c.getCommand();
+			}
+		}
+
+		return command;
+	}
 }
