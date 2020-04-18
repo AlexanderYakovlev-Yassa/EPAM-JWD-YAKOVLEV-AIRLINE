2c04df14c180e337f0587204dd3bd8ca11a5470f
Alexander Yakovlev <sashayassa@gmail.com>
2020-04-18 12:55:02 +0300

    update page enum

--- src/main/java/by/epam/jwd/yakovlev/airline/command/PageEnum.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/command/PageEnum.java
@@ -1,19 +1,42 @@
 package by.epam.jwd.yakovlev.airline.command;
 
 public enum PageEnum {
+    AIRCRAFTS ("/jsp/Aircrafts.jsp"),
     INDEX ("/jsp/Index.jsp"),
     LOGIN ("/jsp/Login.jsp"),
     REGISTRATION ("/jsp/Registration.jsp"),
     ERROR ("/jsp/error.jsp"),
-    REGISTRATION_SUCCESS ("/jsp/RegistrationSuccess.jsp");
+    REGISTRATION_SUCCESS ("/jsp/RegistrationSuccess.jsp"),
+    EMPLOYEE ("/jsp/Employee.jsp"),
+    CABINET ("/jsp/Cabinet.jsp"),
+    AIRCRAFT_MANAGEMENT ("/jsp/AircraftManagement.jsp"),
+    AIRCRAFT_MODELS_MANAGEMENT ("/jsp/AircraftModelsManagement.jsp");
 
-    private String page;
+    private String pageURL;
 
-    PageEnum(String page) {
-        this.page = page;
+    PageEnum(String pageURL) {
+        this.pageURL = pageURL;
     }
 
-    public String getPage() {
-        return page;
+    public static String getPageURL(String pageName) {
+
+        if (pageName == null) {
+            return null;
+        }
+
+        String URL = null;
+
+        for (PageEnum p : PageEnum.values()) {
+            if (p.name().equals(pageName)) {
+                URL = p.pageURL;
+                break;
+            }
+        }
+
+        return URL;
+    }
+
+    public String getPageURL() {
+        return pageURL;
     }
 }
