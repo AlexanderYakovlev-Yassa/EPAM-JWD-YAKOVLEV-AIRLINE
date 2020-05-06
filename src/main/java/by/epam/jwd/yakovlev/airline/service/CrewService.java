dbf83df70fea3a8a8a719b49199ef6ac3d691d12
Alexander Yakovlev <sashayassa@gmail.com>
2020-05-06 10:41:42 +0300

    Refactor interface CrewService

--- src/main/java/by/epam/jwd/yakovlev/airline/service/CrewService.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/service/CrewService.java
@@ -1,9 +1,12 @@
 package by.epam.jwd.yakovlev.airline.service;
 
+import by.epam.jwd.yakovlev.airline.entity.Crew;
 import by.epam.jwd.yakovlev.airline.exception.ServiceException;
 
 public interface CrewService {
 	
 	boolean addCrewMember(int flightID, int employeeID) throws ServiceException;
 	boolean deleteCrewMember(int flightID, int employeeID) throws ServiceException;
+	
+	Crew getCrewByFlightID(int flightID) throws ServiceException;
 }
