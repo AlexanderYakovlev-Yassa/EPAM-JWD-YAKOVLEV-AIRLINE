8122fe6a10d0e1088664fb4d4402b246ba10f02e
Alexander Yakovlev <sashayassa@gmail.com>
2020-05-06 10:27:46 +0300

    Add command: GotoPageLogin

---
+++ src/main/java/by/epam/jwd/yakovlev/airline/command/impl/GotoPageLogin.java
@@ -0,0 +1,19 @@
+package by.epam.jwd.yakovlev.airline.command.impl;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import by.epam.jwd.yakovlev.airline.command.AbstractCommand;
+import by.epam.jwd.yakovlev.airline.command.PageEnum;
+
+public class GotoPageLogin extends AbstractCommand {
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
+		return PageEnum.LOGIN.getPageURL();
+	}
+
+}
