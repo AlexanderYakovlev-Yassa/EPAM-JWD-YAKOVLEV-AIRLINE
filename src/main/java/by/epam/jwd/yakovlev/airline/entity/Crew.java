0664c82faf9b52afb8ec717b943470207350d227
Alexander Yakovlev <sashayassa@gmail.com>
2020-05-06 10:36:20 +0300

    Refactor Crew

--- src/main/java/by/epam/jwd/yakovlev/airline/entity/Crew.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/entity/Crew.java
@@ -4,65 +4,58 @@ import java.util.List;
 
 public class Crew {
 
-    private int crewID;
+	private int flightID;
     private List<Employee> crewMembersList;
-    private Flight flight;
 
     public Crew() {
     }
 
-    public int getCrewID() {
-        return crewID;
-    }
+	public int getFlightID() {
+		return flightID;
+	}
 
-    public void setCrewID(int crewID) {
-        this.crewID = crewID;
-    }
+	public void setFlightID(int flightID) {
+		this.flightID = flightID;
+	}
 
-    public List<Employee> getCrewMembersList() {
-        return crewMembersList;
-    }
+	public List<Employee> getCrewMembersList() {
+		return crewMembersList;
+	}
 
-    public void setCrewMembersList(List<Employee> crewMembersList) {
-        this.crewMembersList = crewMembersList;
-    }
+	public void setCrewMembersList(List<Employee> crewMembersList) {
+		this.crewMembersList = crewMembersList;
+	}
 
-    public Flight getFlight() {
-        return flight;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((crewMembersList == null) ? 0 : crewMembersList.hashCode());
+		result = prime * result + flightID;
+		return result;
+	}
 
-    public void setFlight(Flight flight) {
-        this.flight = flight;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Crew other = (Crew) obj;
+		if (crewMembersList == null) {
+			if (other.crewMembersList != null)
+				return false;
+		} else if (!crewMembersList.equals(other.crewMembersList))
+			return false;
+		if (flightID != other.flightID)
+			return false;
+		return true;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Crew crew = (Crew) o;
-
-        if (crewID != crew.crewID) return false;
-        if (crewMembersList != null ? !crewMembersList.equals(crew.crewMembersList) : crew.crewMembersList != null)
-            return false;
-        return flight != null ? flight.equals(crew.flight) : crew.flight == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = crewID;
-        result = 31 * result + (crewMembersList != null ? crewMembersList.hashCode() : 0);
-        result = 31 * result + (flight != null ? flight.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        final StringBuffer sb = new StringBuffer("Crew{");
-        sb.append("crewID=").append(crewID);
-        sb.append(", crewMembersList=").append(crewMembersList);
-        sb.append(", flight=").append(flight);
-        sb.append('}');
-        return sb.toString();
-    }
+	@Override
+	public String toString() {
+		return "Crew [flightID=" + flightID + ", crewMembersList=" + crewMembersList + "]";
+	}
 }
