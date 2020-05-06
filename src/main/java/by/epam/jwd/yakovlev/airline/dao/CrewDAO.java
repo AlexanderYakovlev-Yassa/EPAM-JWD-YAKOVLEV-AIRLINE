2fd352b770f8f4a4379e54fe3f858b35a78db488
Alexander Yakovlev <sashayassa@gmail.com>
2020-05-06 10:40:41 +0300

    Refactor interface CrewDAO

--- src/main/java/by/epam/jwd/yakovlev/airline/dao/CrewDAO.java
+++ src/main/java/by/epam/jwd/yakovlev/airline/dao/CrewDAO.java
@@ -1,9 +1,13 @@
 package by.epam.jwd.yakovlev.airline.dao;
 
+import java.util.Optional;
+
+import by.epam.jwd.yakovlev.airline.entity.Crew;
 import by.epam.jwd.yakovlev.airline.exception.DaoException;
 
 public interface CrewDAO {
 
 	boolean addCrewMember(int flightID, int employeeID) throws DaoException;
 	boolean deleteCrewMember(int flightID, int employeeID) throws DaoException;
+	Optional<Crew> getCrewByFlightID(int flightID) throws DaoException;
 }
